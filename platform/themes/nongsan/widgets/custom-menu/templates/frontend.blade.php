c53a50ab18eba2110d97d2473a7e2cc873d6a54f
tuypq <quoctuy8298@gmail.com>
2021-10-30 01:00:15 +0700

    fix giao dien

--- platform/themes/nongsan/widgets/custom-menu/templates/frontend.blade.php
+++ platform/themes/nongsan/widgets/custom-menu/templates/frontend.blade.php
@@ -1,21 +1,12 @@
-@if ($sidebar == 'footer_sidebar')
-    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-        <aside class="widget widget--transparent widget__footer">
-            @else
-                <aside class="widget widget--transparent">
-                    @endif
-                    <div class="widget__header">
-                        <h3 class="widget__title">{{ $config['name'] }}</h3>
-                    </div>
-                    <div class="widget__content">
-                        {!!
+<div class="page-sidebar">
+    <aside class="widget widget_product_categories">
+        <h3 class="widget-title">{{ $config['name'] }}</h3>
+        {!!
                             Menu::generateMenu([
                                 'slug'    => $config['menu_id'],
-                                'options' => ['class' => ($config['menu_id'] == 'social' ? 'social social--simple social--widget' : 'list list--fadeIn') . ($sidebar == 'footer_sidebar' ? ' list--light' : '') ]
+                                'options' => 'class = "product-categories"',
+                                'view'=>'menu/categories'
                             ])
                         !!}
-                    </div>
-                </aside>
-        @if ($sidebar == 'footer_sidebar')
-    </div>
-@endif
+    </aside>
+</div>
--- platform/themes/nongsan/widgets/recent-posts/templates/frontend.blade.php
+++ platform/themes/nongsan/widgets/recent-posts/templates/frontend.blade.php
@@ -3,38 +3,23 @@
         $posts = get_recent_posts($config['number_display']);
     @endphp
     @if ($posts->count())
-        @if ($sidebar == 'footer_sidebar')
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                <div class="widget widget--transparent widget__footer">
-                    @else
-                        <div class="widget widget__recent-post">
-                            @endif
-                            <div class="widget__header">
-                                <h3 class="widget__title">{{ $config['name'] }}</h3>
+        <div class="widget">
+            <h5 class="widget_title">{{ $config['name'] }}</h5>
+            <ul class="widget_recent_post">
+                @foreach ($posts as $post)
+                    <li>
+                        <div class="post_footer">
+                            <div class="post_img">
+                                <a href="{{ $post->url }}"><img src="{{ RvMedia::getImageUrl($post->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $post->name }}"></a>
                             </div>
-                            <div class="widget__content">
-                                <ul @if ($sidebar == 'footer_sidebar') class="list list--light list--fadeIn" @endif>
-                                    @foreach ($posts as $post)
-                                        <li>
-                                            @if ($sidebar == 'footer_sidebar')
-                                                <a href="{{ $post->url }}" data-number-line="2">{{ $post->name }}</a>
-                                            @else
-                                                <article class="post post__widget clearfix">
-                                                    <div class="post__thumbnail"><img src="{{ RvMedia::getImageUrl($post->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $post->name }}">
-                                                        <a href="{{ $post->url }}" class="post__overlay"></a></div>
-                                                    <header class="post__header">
-                                                        <h5 class="post__title"><a href="{{ $post->url }}" data-number-line="2">{{ $post->name }}</a></h5>
-                                                        <div class="post__meta"><span class="post__created-at">{{ $post->created_at->format('M d, Y') }}</span></div>
-                                                    </header>
-                                                </article>
-                                            @endif
-                                        </li>
-                                    @endforeach
-                                </ul>
+                            <div class="post_content">
+                                <h6><a href="{{ $post->url }}">{{ $post->name }}</a></h6>
+                                <p class="small m-0">{{ $post->created_at->translatedFormat('M d, Y') }}</p>
                             </div>
                         </div>
-                        @if ($sidebar == 'footer_sidebar')
-                </div>
-        @endif
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     @endif
 @endif
