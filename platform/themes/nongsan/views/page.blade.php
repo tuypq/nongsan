67a2a16c9dcb930546ae9e377ad61c4f43753d60
tuypq <quoctuy8298@gmail.com>
2021-10-30 02:02:31 +0700

    fix

--- platform/themes/nongsan/views/page.blade.php
+++ platform/themes/nongsan/views/page.blade.php
@@ -9,7 +9,7 @@
 </div>
 <div class="container container-ver2">
     <div class="blog-post-container blog-page blog-classic">
-        <div id="primary" class="col-xs-12 col-md-9 float-left center">
+        <div id="primary" class="float-left center">
             <div class="blog-post-container box single-post">
                 <div class="blog-post-item">
                     <div class="head">
@@ -23,46 +23,6 @@
                     <div class="text align-left">
                         {!!$page->content  !!}
 
-                        <!-- End share -->
-{{--                        <div class="box">--}}
-{{--                            <h3 class="title-v1 space-30">Related Posts</h3>--}}
-{{--                            <div class="blog-post-inner blog-related owl-nav-hidden center owl-carousel">--}}
-{{--                                <div class="blog-post-item">--}}
-{{--                                    <div class="blog-post-image hover-images">--}}
-{{--                                        <a href="#" title="Post"><img src="assets/images/products/1.jpg" alt=""></a>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="text">--}}
-{{--                                        <h3><a href="#" title="Integer scelerisque diam vitae aliquam fringilla.">The new Brand Identity last the Bahamas</a></h3>--}}
-{{--                                        <p class="post-by"><span><i class="fa fa-pencil-square-o"></i> Admin</span><span><i class="fa fa-comment-o"></i> 36 Comments</span></p>--}}
-{{--                                    </div>--}}
-{{--                                    <!-- End text -->--}}
-{{--                                </div>--}}
-{{--                                <!-- End item -->--}}
-{{--                                <div class="blog-post-item">--}}
-{{--                                    <div class="blog-post-image hover-images">--}}
-{{--                                        <a href="#" title="Post"><img src="assets/images/products/1.jpg" alt=""></a>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="text">--}}
-{{--                                        <h3><a href="#" title="Integer scelerisque diam vitae aliquam fringilla.">The new Brand Identity last the Bahamas</a></h3>--}}
-{{--                                        <p class="post-by"><span><i class="fa fa-pencil-square-o"></i> Admin</span><span><i class="fa fa-comment-o"></i> 36 Comments</span></p>--}}
-{{--                                    </div>--}}
-{{--                                    <!-- End text -->--}}
-{{--                                </div>--}}
-{{--                                <!-- End item -->--}}
-{{--                                <div class="blog-post-item">--}}
-{{--                                    <div class="blog-post-image hover-images">--}}
-{{--                                        <a href="#" title="Post"><img src="assets/images/products/1.jpg" alt=""></a>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="text">--}}
-{{--                                        <h3><a href="#" title="Integer scelerisque diam vitae aliquam fringilla.">The new Brand Identity last the Bahamas</a></h3>--}}
-{{--                                        <p class="post-by"><span><i class="fa fa-pencil-square-o"></i> Admin</span><span><i class="fa fa-comment-o"></i> 36 Comments</span></p>--}}
-{{--                                    </div>--}}
-{{--                                    <!-- End text -->--}}
-{{--                                </div>--}}
-{{--                                <!-- End item -->--}}
-{{--                            </div>--}}
-{{--                            <!-- End Blog3ItemSlider -->--}}
-{{--                        </div>--}}
                     </div>
                     <!-- End text -->
                 </div>
@@ -71,14 +31,7 @@
             <!-- End blog-post-container -->
         </div>
         <!-- End Primary -->
-        <div id="secondary" class="widget-area col-xs-12 col-md-3 float-right">
-            <div class="wrap-sidebar">
-                <aside class="widget widget_product_categories">
-                    {!! dynamic_sidebar('primary_sidebar') !!}
-                </aside>
-            </div>
-            <!-- End wrap-sidebar -->
-        </div>
+
         <!-- End Secondary -->
     </div>
     <!-- end product sidebar -->
